48559b1c9f23921eef312f8863d897ccaab84331
Davide Termite <davide.termite@gmail.com>
2020-05-25 18:25:02 +0200

    Gandalf likes to play dices

--- js/dadi.js
+++ js/dadi.js
@@ -10,11 +10,37 @@ var secondDicePlayer = Math.floor(Math.random() * 6) + 1;
 var computer = firstDiceComputer + secondDiceComputer;
 var player = firstDicePlayer + secondDicePlayer;
 
+// Variabile per immagini
+var gandalfWin = document.getElementById('gandalfYay');
+var gandalfLose = document.getElementById('gandalfNay');
+var gandalfDraw = document.getElementById('gandalfDraw');
+
+var gandalfTextWin = document.getElementById('textYay');
+var gandalfTextLose = document.getElementById('textNay');
+var gandalfTextDraw = document.getElementById('textDraw');
+
+var text = document.getElementById('question');
+
 // LET THE GAME BEGIN
 if (computer == player) {
-  alert("pari");
+  text.innerHTML = "Il tuo numero è " + player + ". <br> Il numero di Gandalf è " + computer;
+
+  gandalfDraw.className = "visible";
+  gandalfTextDraw.innerHTML = "Pareggio!";
+  gandalfTextDraw.className = "visible";
+
 } else if (computer > player) {
-  alert("hai perso");
-} else {
-  alert("hai vinto");
-}
+  text.innerHTML = "Il tuo numero è " + player + ". <br> Il numero di Gandalf è " + computer;
+
+  gandalfWin.className = "visible";
+  gandalfTextWin.innerHTML = "Hai perso!";
+  gandalfTextWin.className = "visible";
+
+  } else {
+  text.innerHTML = "Il tuo numero è " + player + ". <br> Il numero di Gandalf è " + computer;
+
+  gandalfLose.className = "visible";
+  gandalfTextLose.innerHTML = "Hai vinto!";
+  gandalfTextLose.className = "visible";
+
+  }
