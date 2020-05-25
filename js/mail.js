e06cc5f622001149c7e2c98da5782153ec3ace6a
Davide Termite <davide.termite@gmail.com>
2020-05-25 17:52:38 +0200

    Gandalf is angry

--- js/mail.js
+++ js/mail.js
@@ -15,9 +15,23 @@ for (var i = 0; i < mailArray.length; i++) {
   }
 }
 
+// Variabili per Img
+var gandalfTrue = document.getElementById('gandalfYay');
+var gandalfFalse = document.getElementById('gandalfNay');
+
+// Variabili per Text
+var gandalfQuestion = document.getElementById('question');
+var gandalfTextTrue = document.getElementById('textYay');
+var gandalfTextFalse = document.getElementById('textNay');
+
 // Notifica per l'utente
 if (check === true) {
-  alert ("Benvenuto");
+  gandalfQuestion.className = "hidden";
+  gandalfTrue.className = "visible";
+  gandalfTextTrue.className = "visible";
+
 } else {
-  alert ("Inserisci mail valida");
+  gandalfQuestion.className = "hidden";
+  gandalfFalse.className = "visible";
+  gandalfTextFalse.className = "visible";
 }
